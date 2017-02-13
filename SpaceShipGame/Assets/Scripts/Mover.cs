976c6ae94014b8457973806567864b8a213b582d
Lainezz <diegolinaresmojacar@gmail.com>
2017-02-13 16:22:49 +0100

    Subida SpaceShipGame
    Primer commit de spaceshipgame. Aun en desarrollo

---
+++ SpaceShipGame/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    private Rigidbody boltRb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        boltRb = GetComponent<Rigidbody>();
+        boltRb.velocity = transform.forward * speed;
+    }
+}
