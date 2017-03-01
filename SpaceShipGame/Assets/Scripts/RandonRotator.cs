ba60c2f3e4ef7aa6fc2886aaedaa167a1f293996
Lainezz <diegolinaresmojacar@gmail.com>
2017-03-01 13:15:03 +0100

    Ultima subida SpaceShip Game

---
+++ SpaceShipGame/Assets/Scripts/RandonRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandonRotator : MonoBehaviour {
+
+    public float tumble;
+
+    private Rigidbody asteroid;
+
+    void Start()
+    {
+        asteroid = GetComponent<Rigidbody>();
+        asteroid.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
